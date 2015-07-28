c164eeaf779410c99e9df5ea1b3e01dd642180bd
GloomyMouse Xu <gloomymouse@126.com>
2015-07-28 20:23:56 +0800

    ZigZag Conversion
    The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
    
    P   A   H   N
    A P L S I I G
    Y   I   R
    And then read line by line: "PAHNAPLSIIGYIR"
    Write the code that will take a string and make this conversion given a number of rows:
    
    string convert(string text, int nRows);
    convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".

---
+++ 006.c
@@ -0,0 +1,43 @@
+char* convert(char* s, int numRows) 
+{
+	int num = strlen(s);
+	if (num == 0 || numRows == 1)
+        return s;
+
+	char* sFinal;
+	sFinal = (char*)malloc(num + 1);
+
+	int index;
+	int i = 0;
+	int n = 0;
+
+	for (i=0; i<numRows; i++)
+	{
+		index = i;
+		if (index >= num)
+			break;
+		while (1)
+		{		
+			if (i != numRows - 1)
+			{
+				sFinal[n] = s[index];
+				n++;
+				index = index + (numRows - 1 - i) * 2;
+				if (index >= num)
+					break;
+			}
+		
+			if(i != 0 )
+			{
+				sFinal[n] = s[index];
+				n++;
+				index = index + i * 2;
+				if (index >= num)
+					break;
+			}
+		}
+	}
+	sFinal[n] = '\0';
+
+	return sFinal;
+}
