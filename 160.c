a32be2a832bbde3f3b7ffa18c91645799d591efb
GloomyMouse Xu <gloomymouse@126.com>
2016-01-04 21:34:44 +0800

    ntersection of Two Linked Lists
    Write a program to find the node at which the intersection of two singly linked lists begins.
    
    
    For example, the following two linked lists:
    
    A:               a1 → a2 →                 
                                         c1 → c2 → c3  
    B:     b1 → b2 → b3 →
    begin to intersect at node c1.
    
    
    Notes:
    
    If the two linked lists have no intersection at all, return null.
    The linked lists must retain their original structure after the function returns.
    You may assume there are no cycles anywhere in the entire linked structure.
    Your code should preferably run in O(n) time and use only O(1) memory.

---
+++ 160.c
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB)
+{
+    if (headA == NULL || headB == NULL)
+        return NULL;
+    
+    // convert the List B to a loop
+    struct ListNode *cur = headB;
+    while (cur->next != NULL)
+        cur = cur->next;
+    struct ListNode *tail = cur;
+    cur->next = headB;
+    
+    // if the List A has a loop, there is a interscetion
+    cur = headA;
+    struct ListNode *fast = headA;
+    while (1)
+    {
+        if (fast == NULL || fast->next == NULL)
+        {
+            tail->next = NULL;  // convert the List B back to a single linked list
+            return NULL;
+        }
+        else
+        {
+            cur = cur->next;
+            fast = fast->next->next;
+            if (cur == fast)
+                break;
+        }
+    }
+
+    cur = headA;
+    while (1)
+    {
+        if (cur == fast)
+        {
+            tail->next = NULL;  // convert the List B back to a single linked list
+            return cur;
+        }
+        else
+        {
+            cur = cur->next;
+            fast = fast->next;
+        }
+    }
+}
