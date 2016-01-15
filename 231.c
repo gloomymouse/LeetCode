f9052b3897e6f2e9f3303f13c968dfa0acf00e51
GloomyMouse Xu <gloomymouse@126.com>
2016-01-15 19:40:18 +0800

    Power of Two
    Given an integer, write a function to determine if it is a power of two.

---
+++ 231.c
@@ -0,0 +1,11 @@
+bool isPowerOfTwo(int n)
+{
+    if (n < 1)
+        return false;
+    while (n % 2 == 0)
+        n /= 2;
+    if (n == 1)
+        return true;
+    else
+        return false;
+}
