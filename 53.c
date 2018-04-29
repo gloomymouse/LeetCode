b43609811da0e890b36ae31f232718ba5d10e18e
GloomyMouse Xu <gloomymouse@126.com>
2018-04-30 05:30:36 +0800

    Maximum Subarray
    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
    
    Example:
    
    Input: [-2,1,-3,4,-1,2,1,-5,4],
    Output: 6
    Explanation: [4,-1,2,1] has the largest sum = 6.
    Follow up:
    
    If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.

---
+++ 53.c
@@ -0,0 +1,30 @@
+int maxSubArray(int* nums, int numsSize) 
+{    
+    int iMaxSum = 0;
+    int *piSum;
+    
+    piSum = (int *)malloc(sizeof(int)*numsSize);
+    memset(piSum, 0, (sizeof(int)*numsSize));
+    
+    piSum[0] = nums[0];
+    iMaxSum  = nums[0];
+    
+    for (int i = 1; i < numsSize; i++)
+    {
+        if (piSum[i-1] >= 0)
+        {
+            piSum[i] = piSum[i-1] + nums[i];
+        }
+        else
+        {
+            piSum[i] = nums[i];
+        }
+        
+        if (iMaxSum < piSum[i])
+        {
+            iMaxSum = piSum[i];
+        }
+    }
+    
+    return iMaxSum;
+}
