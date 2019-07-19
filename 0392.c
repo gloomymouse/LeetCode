f78bf3d637219cc813fdccdf09679da3a82b25df
GloomyMouse Xu <gloomymouse@126.com>
2019-07-19 16:15:59 +0800

    Is Subsequence
    Given a string s and a string t, check if s is subsequence of t.
    
    You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
    
    A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
    
    Example 1:
    s = "abc", t = "ahbgdc"
    
    Return true.
    
    Example 2:
    s = "axc", t = "ahbgdc"
    
    Return false.
    
    Follow up:
    If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?

---
+++ 0392.c
@@ -0,0 +1,14 @@
+bool isSubsequence(char * s, char * t) {
+    int i = 0;
+    int j = 0;
+    while (s[i] != '\0' && t[j] != '\0') {
+        if (s[i] == t[j]) {
+            i++;
+        }
+        j++;
+    }
+    if (s[i] == '\0') {
+        return true;
+    }
+    return false;
+}
