35990ed78c44a8dde5910c2208756309b1ec0789
GloomyMouse Xu <gloomymouse@126.com>
2019-08-03 22:31:40 +0800

    H-Index
    Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
    
    According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
    
    Example:
    
    Input: citations = [3,0,6,1,5]
    Output: 3 
    Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had 
                 received 3, 0, 6, 1, 5 citations respectively. 
                 Since the researcher has 3 papers with at least 3 citations each and the remaining 
                 two with no more than 3 citations each, her h-index is 3.
    Note: If there are several possible values for h, the maximum one is taken as the h-index.

---
+++ 0274.c
@@ -0,0 +1,13 @@
+int comp(const void* a, const void* b) {
+    return (*(int*)b) - (*(int*)a);
+}
+
+int hIndex(int* citations, int citationsSize){
+    qsort(citations, citationsSize, sizeof(int), comp);
+    for (int i = 0; i < citationsSize; i++) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+    return citationsSize;
+}
